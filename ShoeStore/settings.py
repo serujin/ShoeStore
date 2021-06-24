6daf8f28e3f1b95519448a92c7cbc1fabd63400e
Serujin <sergio.munoz.lillo@gmail.com>
2021-06-24 19:27:01 +0200

    Added cart functionality, buy option isnt done yet

--- ShoeStore/settings.py
+++ ShoeStore/settings.py
@@ -60,7 +60,10 @@ ROOT_URLCONF = 'ShoeStore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'home/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- static/js/home.js
+++ static/js/home.js
@@ -3,15 +3,90 @@ function toggleCart() {
 }
 
 function addProductToCart(trigger) {
-    id = trigger.parentNode.parentNode.childNodes[1].innerHTML
-    name = trigger.parentNode.parentNode.childNodes[3].innerHTML
-    price = trigger.parentNode.parentNode.childNodes[7].innerHTML
-    console.log("Added", id, name, price)
+    cartProducts = document.getElementsByClassName("cart-item");
+    product = trigger.parentNode.parentNode.childNodes[1].childNodes[1].innerHTML;
+    price = trigger.parentNode.childNodes[1].childNodes[1].innerHTML;
+    exists = false;
+    Array.from(cartProducts).forEach(cartItem => {
+        itemName = cartItem.childNodes[0].innerHTML;
+        if(itemName == product) {
+            itemPrice = cartItem.childNodes[2].childNodes[0].innerHTML;
+            if(itemPrice == price) {
+                itemQuantitySpan = cartItem.childNodes[1];
+                itemQuantity = +(itemQuantitySpan.innerHTML) + 1;
+                itemQuantitySpan.innerHTML = itemQuantity;
+                exists = true;
+                return; 
+            }
+        } 
+    });
+    if(!exists) {
+        cart = document.getElementById("cart-body");
+        cart.appendChild(getCartHolder(product, price));
+    }
+    updateCart();
 }
 
 function removeProductFromCart(trigger) {
-    id = trigger.parentNode.parentNode.childNodes[1].innerHTML
-    name = trigger.parentNode.parentNode.childNodes[3].innerHTML
-    price = trigger.parentNode.parentNode.childNodes[7].innerHTML
-    console.log("Removed", id, name, price)
+    itemQuantitySpan = trigger.parentNode.childNodes[1];
+    itemQuantity = +(itemQuantitySpan.innerHTML) - 1;
+    itemQuantitySpan.innerHTML = itemQuantity;
+    if(itemQuantity == 0) {
+        trigger.parentNode.parentNode.removeChild(trigger.parentNode)
+    }
+    updateCart();
 }
+
+function updateCart() {
+    cartProducts = document.getElementsByClassName("cart-item");
+    if(cartProducts.length == 0) {
+        document.getElementById("cart-header").style = "display: none !important;"
+        document.getElementById("cart-footer").style = "display: none !important;"
+        document.getElementById("cart-body-empty").style = "display: flex !important"
+    } else {
+        document.getElementById("cart-header").style = "display: flex !important;"
+        document.getElementById("cart-footer").style = "display: flex !important;"
+        document.getElementById("cart-body-empty").style = "display: none !important"
+        updateTotal();
+    }
+}
+
+function updateTotal() {
+    cartProducts = document.getElementsByClassName("cart-item");
+    total = 0.00;
+    Array.from(cartProducts).forEach(cartItem => {
+        itemQuantity = cartItem.childNodes[1].innerHTML;
+        itemPrice = cartItem.childNodes[2].childNodes[0].innerHTML;
+        total += +(itemQuantity) * +(itemPrice);
+    });
+    document.getElementById("total-price").innerHTML = (Math.round(total * 100) / 100).toFixed(2);
+}
+
+function getCartHolder(name, price) {
+    holder = create("div", "", "row cart-item dropdown-item d-flex align-items-center m-0");
+    start = create("p", name, "m-0 col-5 text-truncate text-center");
+    mid = create("p", "1", "m-0 px-1 col-3 text-center");
+    end = create("p", "<span>" + price + "</span>€", "m-0 col-3 text-center")
+    button = create("button", '<i class="fas fa-trash-alt">', "btn btn-outline-danger col-1");
+    button.setAttribute("onclick", "removeProductFromCart(this)");
+    holder.appendChild(start);
+    holder.appendChild(mid);
+    holder.appendChild(end);
+    holder.appendChild(button);
+    return holder;
+}
+
+function create(type, inner="", classes="") {
+    element = document.createElement(type);
+    if(inner.length > 0) {
+        element.innerHTML = inner;
+    }
+    if(classes.length > 0) {
+        element.setAttribute("class", classes);
+    }
+    return element;
+}
+
+document.getElementById("cart").addEventListener('click', function (event) {
+    event.stopPropagation();
+});
